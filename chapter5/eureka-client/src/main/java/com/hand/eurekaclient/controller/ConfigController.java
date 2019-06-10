98ebdd23ce87c8499893f33426b81b32b9dbe5f2
19981 <zhiying.dong@hand-china.com>
2019-06-10 09:04:48 +0800

    [FIX]spring cloud config

--- chapter5/eureka-client/src/main/java/com/hand/eurekaclient/controller/ConfigController.java
+++ chapter5/eureka-client/src/main/java/com/hand/eurekaclient/controller/ConfigController.java
@@ -1,5 +1,8 @@
 package com.hand.eurekaclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -8,12 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
  * @author zhiying.dong@hand-china.com 2019/03/30 18:57
  */
 @RestController
+@RefreshScope
 public class ConfigController {
-  /*  @Value("${name}")
-    private String name;*/
+    @Value("${name}")
+    private String name;
 
-   /* @GetMapping("/name")
+    @GetMapping("/name")
     public String config() {
         return "I am " + name;
-    }*/
+    }
 }
